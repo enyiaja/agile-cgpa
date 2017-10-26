93187f5dbe96a08e348fc1021f79c2b068e71f64
enyiaja <enyiaja@gmail.com>
2017-10-26 15:58:08 +0100

    Initial commit

---
+++ courses.cpp
@@ -0,0 +1,17 @@
+#include 'courses.h'
+
+
+struct course{
+	string courseTitle[];
+	int courseUnits[];
+};
+
+class courses{
+	private:
+		course courseObject[];
+		int grade[];
+	public:
+		courses::courses(){
+			
+		}
+};
